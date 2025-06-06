3034f7b25248538840d2492dca39d9abf91416e4
Sapphyra Wiser <sapphyra.wiser@gmail.com>
2025-06-06 14:17:31 -0600

    feat: customize fonts for the application - use Merriweather Sans for headers and Lato for body

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { ThemeProvider } from "@/components/theme-provider";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import { ThemeProvider } from "@/components/ui/theme-provider";
+import { lato, merriweatherSans } from "@/styles/fonts";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body className={`${merriweatherSans.variable} ${lato.variable}`}>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
---
+++ styles/fonts.ts
@@ -0,0 +1,12 @@
+import { Lato, Merriweather_Sans } from "next/font/google";
+
+const merriweatherSans = Merriweather_Sans({
+  variable: "--font-merriweather-sans",
+});
+
+const lato = Lato({
+  variable: "--font-lato",
+  weight: ["400", "700"],
+});
+
+export { merriweatherSans, lato };
