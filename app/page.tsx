f412676277c9028d31563d76bda6fca05dae915c
Sapphyra Wiser <sapphyra.wiser@gmail.com>
2025-06-06 13:43:36 -0600

    feat: create dark mode toggle

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import ColorModeToggle from "@/components/ui/color-mode-toggle";
+
 export default function Home() {
-  return <div />;
+  return <ColorModeToggle />;
 }
---
+++ components/ui/color-mode-toggle.tsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+import { Moon, Sun } from "lucide-react";
+import { useTheme } from "next-themes";
+import { Button } from "./button";
+
+const ColorModeToggle = () => {
+  const [mounted, setMounted] = useState(false);
+
+  // avoid hydration mismatch warning
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  const { setTheme, theme } = useTheme();
+  const toggleTheme = () =>
+    setTheme((prev) => (prev === "light" ? "dark" : "light"));
+  const Icon = theme === "light" ? Moon : Sun;
+
+  if (!mounted) return null;
+
+  return (
+    <Button variant="outline" size="icon" onClick={toggleTheme}>
+      <Icon />
+      <span className="sr-only">Toggle theme</span>
+    </Button>
+  );
+};
+
+export default ColorModeToggle;
