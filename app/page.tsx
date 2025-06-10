f78c217cce8df61155f87284d491d8e7a8ba2d6f
Sapphyra Wiser <sapphyra.wiser@gmail.com>
2025-06-10 15:32:38 -0600

    feat: enhance Home component layout and add description

--- app/page.tsx
+++ app/page.tsx
@@ -3,5 +3,14 @@
 import ColorModeToggle from "@/components/ui/color-mode-toggle";
 
 export default function Home() {
-  return <ColorModeToggle />;
+  return (
+    <div className="flex-1 w-full">
+      <ColorModeToggle />
+      <h1>Custom resume generator</h1>
+      <p>
+        Utility to make passing the ATS more feasible. Allows for customization
+        of sections while abstracting formatting logic.
+      </p>
+    </div>
+  );
 }
