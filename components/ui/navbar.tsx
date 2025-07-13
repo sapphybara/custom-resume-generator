02883a0e1e2ccf0d08b7c7506c0a1193e5d0798e
Sapphyra Wiser <sapphyra.wiser@gmail.com>
2025-07-13 14:47:40 -0600

    refactor: fix location of content in navbar nesting

--- components/ui/navbar.tsx
+++ components/ui/navbar.tsx
@@ -9,33 +9,35 @@ import {
 } from "./navigation-menu";
 
 const Navbar = () => (
-  <NavigationMenu className="flex justify-between w-full py-2">
+  <div className="flex justify-between w-full py-2 items-center">
     <p>Custom resume generator</p>
-    <NavigationMenuList>
-      <NavigationMenuItem>
-        <NavigationMenuLink asChild>
-          <Link href="/">Home</Link>
-        </NavigationMenuLink>
-      </NavigationMenuItem>
-      <NavigationMenuItem>
-        <NavigationMenuTrigger>Generate</NavigationMenuTrigger>
-        <NavigationMenuContent>
-          <ul className="grid w-[400px] gap-2 md:w-[500px] md:grid-cols-2 lg:w-[600px]">
-            <li>
-              <NavigationMenuLink asChild>
-                <Link href="/generate">Use the generator</Link>
-              </NavigationMenuLink>
-            </li>
-            <li>
-              <NavigationMenuLink asChild>
-                <Link href="/sample">Try a sample</Link>
-              </NavigationMenuLink>
-            </li>
-          </ul>
-        </NavigationMenuContent>
-      </NavigationMenuItem>
-    </NavigationMenuList>
-  </NavigationMenu>
+    <NavigationMenu viewport={false}>
+      <NavigationMenuList>
+        <NavigationMenuItem>
+          <NavigationMenuLink asChild>
+            <Link href="/">Home</Link>
+          </NavigationMenuLink>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <NavigationMenuTrigger>Generate</NavigationMenuTrigger>
+          <NavigationMenuContent>
+            <ul className="flex flex-col gap-1 whitespace-nowrap">
+              <li>
+                <NavigationMenuLink asChild>
+                  <Link href="/generate">Use the generator</Link>
+                </NavigationMenuLink>
+              </li>
+              <li>
+                <NavigationMenuLink asChild>
+                  <Link href="/sample">Try a sample</Link>
+                </NavigationMenuLink>
+              </li>
+            </ul>
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+    </NavigationMenu>
+  </div>
 );
 
 export default Navbar;
