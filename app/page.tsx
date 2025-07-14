e0a705102b7587fd59f0b817b6c2893eb5673b68
Sapphyra Wiser <sapphyra.wiser@gmail.com>
2025-07-14 16:04:15 -0600

    feat: enhance Home component layout and add navigation button

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,17 @@
-"use client";
-
-import ColorModeToggle from "@/components/ui/color-mode-toggle";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex-1 w-full">
-      <h1>Custom resume generator</h1>
+    <div className="flex-1 w-full flex flex-col items-center justify-center p-4 gap-4">
+      <h1 className="font-bold text-4xl">Custom resume generator</h1>
       <p>
         Utility to make passing the ATS more feasible. Allows for customization
         of sections while abstracting formatting logic.
       </p>
+      <Button asChild>
+        <Link href="/generate">Get started</Link>
+      </Button>
     </div>
   );
 }
