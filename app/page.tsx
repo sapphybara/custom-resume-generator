11e84daf017ead1b53131aba8b76ef7c6eeb6d45
Sapphyra Wiser <sapphyra.wiser@gmail.com>
2025-07-17 10:36:10 -0600

    refactor: improve flex container on home page

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex-1 w-full flex flex-col items-center justify-center p-4 gap-4">
+    <div className="flex-1 w-full flex flex-col items-center justify-center p-4 gap-12 text-center">
       <h1 className="font-bold text-4xl">Custom resume generator</h1>
       <p>
         Utility to make passing the ATS more feasible. Allows for customization
