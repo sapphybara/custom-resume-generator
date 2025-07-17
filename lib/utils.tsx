bf5d3e2a10b802572574e68d9501dc65e9f061fc
Sapphyra Wiser <sapphyra.wiser@gmail.com>
2025-07-17 11:21:30 -0600

    wip: create `pdf` util directory for improved file management

--- lib/utils.tsx
+++ lib/utils.tsx
@@ -1,7 +1,7 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 import { pdf } from "@react-pdf/renderer";
-import { ResumePDF } from "./pdf-generator";
+import { ResumePDF } from "./pdf/pdf-generator";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
