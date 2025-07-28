5dbbb61c13ce29469de6c07ab432c606be6abea8
Sapphyra Wiser <sapphyra.wiser@gmail.com>
2025-07-28 12:48:32 -0600

    feat: add Log Out button in navbar conditionally

--- components/ui/navbar.tsx
+++ components/ui/navbar.tsx
@@ -9,34 +9,46 @@ import {
   NavigationMenuLink,
   NavigationMenuList,
 } from "./navigation-menu";
+import { auth0 } from "@/lib/auth0";
 
-const Navbar = () => (
-  <NavigationMenu
-    className="flex justify-between w-full py-2 items-center sticky top-0 z-10 bg-background"
-    viewport={false}
-  >
-    <NavigationMenuLink
-      asChild
-      className="text-md font-(family-name:--font-merriweather-sans)"
+const Navbar = async () => {
+  const session = await auth0.getSession();
+
+  return (
+    <NavigationMenu
+      className="flex justify-between w-full py-2 items-center sticky top-0 z-10 bg-background"
+      viewport={false}
     >
-      <Link href="/">Custom Resume Generator</Link>
-    </NavigationMenuLink>
-    <div className="flex items-center gap-2">
-      <ColorModeToggle />
-      <NavigationMenuList>
-        <NavigationMenuItem>
-          <NavigationMenuLink asChild>
-            <Link href="/">Home</Link>
-          </NavigationMenuLink>
-        </NavigationMenuItem>
-        <NavigationMenuItem>
-          <NavigationMenuLink asChild>
-            <Link href="/generate">Generate</Link>
-          </NavigationMenuLink>
-        </NavigationMenuItem>
-      </NavigationMenuList>
-    </div>
-  </NavigationMenu>
-);
+      <NavigationMenuLink
+        asChild
+        className="text-md font-(family-name:--font-merriweather-sans)"
+      >
+        <Link href="/">Custom Resume Generator</Link>
+      </NavigationMenuLink>
+      <div className="flex items-center gap-2">
+        <ColorModeToggle />
+        <NavigationMenuList>
+          <NavigationMenuItem>
+            <NavigationMenuLink asChild>
+              <Link href="/">Home</Link>
+            </NavigationMenuLink>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <NavigationMenuLink asChild>
+              <Link href="/generate">Generate</Link>
+            </NavigationMenuLink>
+          </NavigationMenuItem>
+          {session && (
+            <NavigationMenuItem>
+              <NavigationMenuLink asChild>
+                <a href="/auth/logout">Log Out</a>
+              </NavigationMenuLink>
+            </NavigationMenuItem>
+          )}
+        </NavigationMenuList>
+      </div>
+    </NavigationMenu>
+  );
+};
 
 export default Navbar;
