b7d8bcc7e596cf1478c165191999f173dc0f4f2c
Sapphyra Wiser <sapphyra.wiser@gmail.com>
2025-07-28 12:47:44 -0600

    feat: add auth0 for authentication

--- app/generate/page.tsx
+++ app/generate/page.tsx
@@ -1,12 +1,33 @@
 import { Metadata } from "next";
 
 import ResumeFormWrapper from "./resume-form-wrapper";
+import { auth0 } from "@/lib/auth0";
+import { Button } from "@/components/ui/button";
 
 export const metadata: Metadata = {
   title: "Custom Resume Generator | Generate",
 };
 
-export default function Generate() {
+function NotAuthorized() {
+  return (
+    <div className="flex flex-col items-center gap-2">
+      <h2>Please Log In</h2>
+      <br />
+      <Button asChild>
+        <a href="/auth/login?screen_hint=signup&returnTo=/generate">Sign Up</a>
+      </Button>
+      <Button asChild>
+        <a href="/auth/login?returnTo=/generate">Log In</a>
+      </Button>
+    </div>
+  );
+}
+
+export default async function Generate() {
+  const session = await auth0.getSession();
+
+  const Child = session ? ResumeFormWrapper : NotAuthorized;
+
   return (
     <div className="mx-auto p-6 space-y-8 w-full">
       <div className="text-center mb-8">
@@ -17,7 +38,7 @@ export default function Generate() {
           Fill in your information to create a professional resume
         </p>
       </div>
-      <ResumeFormWrapper />
+      <Child />
     </div>
   );
 }
